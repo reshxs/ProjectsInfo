6ad087b6a8ade275d580a1518da6e2ab7b171c1f
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-11-10 20:54:24 +0500

    Models fields
    Добавил поля моделям. :warning: Не созданы отношения!

--- ProjectsInfo/Models/Project.cs
+++ ProjectsInfo/Models/Project.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjectsInfo.Models
 {
     public class Project
     {
-        public Project()
-        {
-        }
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
     }
 }
--- ProjectsInfo/Models/User.cs
+++ ProjectsInfo/Models/User.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+
 namespace ProjectsInfo.Models
 {
     public class User
     {
-        public User()
-        {
-        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FamalyName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        //TODO добавить отношение Mane-to-One с проектом
     }
 }
