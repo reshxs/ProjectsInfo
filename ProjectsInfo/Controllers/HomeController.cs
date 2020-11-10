2a7be2472d6b9dbdecce02ec813ad145968fcec0
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-11-10 19:19:07 +0500

    Revert "first model template"
    This reverts commit 4c486ba828a4e68ae04fd21de356a6ec92875183.

--- ProjectsInfo/Controllers/HomeController.cs
+++ ProjectsInfo/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ProjectsInfo.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
