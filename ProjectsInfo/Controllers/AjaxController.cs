45e08773d4c4c6d7153f8a4d3d9e11838476aef1
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2021-01-14 19:11:55 +0400

    Delete developers + backend

--- ProjectsInfo/Controllers/AjaxController.cs
+++ ProjectsInfo/Controllers/AjaxController.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectsInfo.Data;
 using ProjectsInfo.Models;
+using ProjectsInfo.Models.Api;
 
 namespace ProjectsInfo.Controllers
 {
@@ -23,7 +25,9 @@ namespace ProjectsInfo.Controllers
             {
                 return NotFound();
             }
-            var project = _context.Projects.Find(id);
+            var project = _context.Projects
+                .Include(p => p.DeveloperAssignments)
+                .FirstOrDefault(p => p.ID == id);
 
             if (project == null)
             {
@@ -47,6 +51,73 @@ namespace ProjectsInfo.Controllers
                 })
                 .Where(d => !d.Assigned)
                 .ToList();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDeveloper(int? id, AddedDeveloper addedDeveloper)
+        {
+            if (id == null || id != addedDeveloper.ProjectId)
+                return NotFound();
+
+            var project = await _context.Projects
+                .Include(p => p.DeveloperAssignments)
+                .ThenInclude(d => d.Developer)
+                .FirstOrDefaultAsync(p => p.ID == id);
+
+            if (project == null)
+                return NotFound();
+
+            var developer = await _context.Developers.FindAsync(addedDeveloper.DeveloperId);
+
+            if (developer == null)
+                return NotFound();
+            
+            project.DeveloperAssignments ??= new List<DeveloperAssignment>();
+
+            if (!project.DeveloperAssignments
+                .Select(d => d.DeveloperID)
+                .Contains(addedDeveloper.DeveloperId))
+            {
+                project.DeveloperAssignments.Add(new DeveloperAssignment
+                {
+                    DeveloperID = addedDeveloper.DeveloperId,
+                    ProjectID = addedDeveloper.ProjectId,
+                    Project = project,
+                    Developer = developer
+                });
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return Conflict();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveDeveloper(int? id, AddedDeveloper addedDeveloper)
+        {
+            if (id == null || addedDeveloper.ProjectId != id)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects
+                .Include(p => p.DeveloperAssignments)
+                .FirstOrDefaultAsync(p => p.ID == id);
+
+            project.DeveloperAssignments ??= new List<DeveloperAssignment>();
+
+            if (project.DeveloperAssignments
+                .Select(d => d.DeveloperID)
+                .Contains(addedDeveloper.DeveloperId))
+            {
+                project.DeveloperAssignments.Remove(
+                    project.DeveloperAssignments
+                        .First(d => d.DeveloperID == addedDeveloper.DeveloperId));
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return NotFound();
         }
     }
 }
